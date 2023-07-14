67bf4e346207b89fd02c5deb8639cf740cca2a8b
unknown <krdukiel@interia.pl>
2023-07-14 16:37:43 +0200

    new imges and first navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+        <div style={{display: "flex", height: 70, justifyContent: "space-around", alignItems: "center", position: "absolute", top: 0, backgroundColor: "white", width: "100%", color: "black"}}>
+                <h1>Emperor and his Sons</h1>
+           <div> <p>Lorem ipsum dolor sit.</p> </div>
+
+        </div>
+    )
+}
+export default Navbar;
